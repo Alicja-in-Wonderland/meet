87a13dfddea46c3c0fde3829efe2d22c095ca691
Alicja <alanotowska@yahoo.co.uk>
2023-08-24 22:07:33 +0200

    Fixes typos

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -22,7 +22,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': `*`,
+      'Access-Control-Allow-Origin': "*",
       'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify({
@@ -51,7 +51,7 @@ module.exports.getAccessToken = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': `*`,
+        'Access-Control-Allow-Origin': "*",
         'Access-Control-Allow-Credentials': true,
       },
       body: JSON.stringify(results),
@@ -93,7 +93,7 @@ module.exports.getCalendarEvents = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': `*`,
+        'Access-Control-Allow-Origin': "*",
         'Access-Control-Allow-Credentials': true,
       },
       body: JSON.stringify({ events: results.data.items }),
@@ -103,7 +103,7 @@ module.exports.getCalendarEvents = async (event) => {
     return {
       statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': `*`,
+        'Access-Control-Allow-Origin': "*",
         'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify(error),
