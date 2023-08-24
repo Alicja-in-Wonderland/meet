c80f39889011a5f9b48d455b4a85b5d049d6f398
Alicja <alanotowska@yahoo.co.uk>
2023-08-24 11:06:13 +0200

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -3,7 +3,7 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https://Alicja-in-Wonderland.github.io/meet/"];
+const redirect_uris = ["https://alicja-in-wonderland.github.io/meet/"];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID, CLIENT_SECRET, redirect_uris[0]
