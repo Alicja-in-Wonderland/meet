336b4fc23f0d324d9cfa62cb52ad984e55df2b87
Alicja <alanotowska@yahoo.co.uk>
2023-08-28 21:40:19 +0200

    Adds offline capability

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 import mockData from './mock-data';
+import NProgress from "nprogress";
+
 /**
  *
  * @param {*} events:
@@ -44,6 +46,12 @@ export const getEvents = async () => {
         return mockData;
     }
 
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return events ? JSON.parse(events) : [];
+    }
+
     const token = await getAccessToken();
 
     if (token) {
@@ -52,6 +60,8 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            NProgress.done();
+            localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else return null;
     }
