332d1d1c7d5413e603e60329a0bc35ede6b646da
Alicja <alanotowska@yahoo.co.uk>
2023-08-30 18:48:25 +0200

    Creates a pie chart

---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+const EventGenresChart = ({ events }) => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884d8'];
+
+    useEffect(() => {
+        setData(getData());
+    }, [events]);
+
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        const data = genres.map((genre) => {
+            const filteredEvents = events.filter((event) =>
+                event.summary.includes(genre)
+            );
+
+            return { name: genre, value: filteredEvents.length, fill: colors };
+        });
+
+        return data;
+    };
+
+    const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+        const RADIAN = Math.PI / 180;
+        const radius = outerRadius;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+        const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+        return percent ? (
+            <text
+                x={x}
+                y={y}
+                fill="#8884d8"
+                textAnchor={x > cx ? 'start' : 'end'}
+                dominantBaseline="central"
+            >
+                {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+            </text>
+        ) : null;
+    };
+
+    return (
+        <ResponsiveContainer width="99%" height={400}>
+            <PieChart width={400} height={400}>
+                <Pie
+                    data={data}
+                    nameKey="name"
+                    labelLine={false}
+                    outerRadius={130}
+                    dataKey="value"
+                    label={renderCustomizedLabel}
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                    ))}
+                </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+    );
+};
+
+export default EventGenresChart;
